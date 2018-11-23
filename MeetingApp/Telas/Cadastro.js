2bed2df003f2b3e9e39ce0ff2fc1d426806770f3
Pedro <pedrorodrigues@cjr.org.br>
2018-11-23 00:36:03 -0200

    back-end adicionado a Cadastro.js

--- MeetingApp/Telas/Cadastro.js
+++ MeetingApp/Telas/Cadastro.js
@@ -4,8 +4,73 @@
  import cadastroStyle from '../Style/cadastroStyle';
  import LinearGradient from 'react-native-linear-gradient'
 
+// Coloque o IP do roteador da rede que
+// conecta o servidor aos dispositivos
+// moveis dos usuarios
+var ROTEADOR = 'trabalhodelp.herokuapp.com';
+// Celular Pedro ---> '192.168.43.176:3000'
+// RoteadorCasa Pedro ---> '192.168.0.2:3000'
+// Heroku ---> 'trabalhodelp.herokuapp.com'
+
+
 //type Props = {};
 export default class Cadastro extends Component {//<Props> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      senha: '',
+      fullname: ''
+    }
+  }
+
+  //verifica se o nome de usuario ja existe no bd
+  usuario_ja_registrado(){
+  
+    fetch('http://' + ROTEADOR + '/verifica/', {
+    
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        
+        usuario: this.state.username,
+        
+      }),
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      if(responseJson.length < 1){
+        this.props.navigation.navigate('CadastroPerguntas');
+      }
+      else{
+        alert('nome de usuario ja registrado. Por favor escolha outro...');
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+
+  }
+  
+    
+  
+  handleUsername = (text) => {
+    this.setState({ username: text })
+  }
+  handleSenha = (text) => {
+    this.setState({ senha: text })
+  }
+  handleFullName = (text) => {
+    this.setState({ fullName: text })
+  }
+
+
+
   render() {
     return (
       <ScrollView>
@@ -13,12 +78,13 @@ export default class Cadastro extends Component {//<Props> {
 
           <View style={cadastroStyle.viewPrimaria}>
 
-            <Text style={{alignSelf:'flex-start', borderColor: 'white', borderWidth: 0, color: 'white', fontSize: 20, marginLeft: 25, marginTop: 80}}>Nome de Usuario: </Text>
+            <Text style={{alignSelf:'flex-start', borderColor: 'white', borderWidth: 0, color: 'white', fontSize: 20, marginLeft: 25, marginTop: 80}}>Usuario: </Text>
             <TextInput
               placeholder='Usuario'
               placeholderTextColor= 'white'
               selectionColor='white'
               style={cadastroStyle.buttons}
+              onChangeText = {this.handleUsername}
             />
 
 
@@ -29,6 +95,7 @@ export default class Cadastro extends Component {//<Props> {
               placeholderTextColor= 'white'
               selectionColor='white'
               style={cadastroStyle.buttons}
+              onChangeText = {this.handleSenha}
             />
 
 
@@ -41,12 +108,13 @@ export default class Cadastro extends Component {//<Props> {
               style={cadastroStyle.buttons}
             />
 
-            <Text style={{alignSelf:'flex-start', borderColor: 'white', borderWidth: 0, color: 'white', fontSize: 20, marginLeft: 25, marginTop: 20}}>E-mail: </Text>
+            <Text style={{alignSelf:'flex-start', borderColor: 'white', borderWidth: 0, color: 'white', fontSize: 20, marginLeft: 25, marginTop: 20}}>Nome completo: </Text>
             <TextInput
-              placeholder='E-mail'
+              placeholder='nome'
               placeholderTextColor= 'white'
               selectionColor='white'
               style={cadastroStyle.buttons}
+              onChangeText = {this.handleFullName}
             />
 
             <TouchableOpacity
@@ -55,7 +123,15 @@ export default class Cadastro extends Component {//<Props> {
                 borderRadius: 20, justifyContent: 'center', alignItems: 'center', backgroundColor: '#7ef3e0'
 
               }}
-              onPress={() => this.props.navigation.navigate('CadastroPerguntas')}
+              onPress={() => {
+                if((this.state.username != '') && (this.state.senha != '')){
+
+                  this.usuario_ja_registrado();                 
+
+                }else
+                  alert('nome de usuario ou senha inválidos');
+                }}
+
             >
             <Text style={{color:'white', fontSize: 18}}>Enviar</Text>
             </TouchableOpacity>
